d718cdd1a2da5dedbfa2826b23cbee1a7ca6f025
Roland Levy <38256994+rolandjlevy@users.noreply.github.com>
2021-12-08 23:46:59 +0000

    Added controls

---
+++ notes.md
@@ -0,0 +1,10 @@
+# Notes
+
+- sphere in CSS: https://codepen.io/vikas78/pen/vYEymWd
+
+```css
+  background: radial-gradient(circle at 65% 15%, white 1px, #ebff36 3%, #324700 60%, #ebff36 100%);
+```
+
+```js
+  animation-delay: ${inc}s;
--- script.js
+++ script.js
@@ -1,14 +1,45 @@
-const totalBalls = 20;
-let count = 0;
-const duration = 2;
-
 const $ = (elem) => document.querySelector(elem);
+const $$ = (elem) => document.querySelectorAll(elem);
+
+const totalBalls = 25;
+const ballSize = 20;
+
+let movement = Number($('#movement').value);
+let duration = Number($('#duration').value);
+let count = 0;
+let inc;
 
 while (count++ < totalBalls) {
-  const inc = count * 0.025;
+  inc = count * movement / 5000;
   const ball = document.createElement('div');
-  ball.className = 'ball';
-  ball.style.animationDelay = `${inc}s`;
-  ball.style.animationDuration = `${duration + inc}s`;
+  const options = {
+    id: count,
+    className: 'ball',
+    style: `
+      width: ${ballSize}px;
+      height: ${ballSize * 1.25}px;
+      animation-delay: ${inc}s;
+      animation-duration: ${duration}s;
+    `
+  }
+  Object.assign(ball, options);
   $('.container').appendChild(ball);
 }
+
+$('#movement').addEventListener('input', e => {
+  movement = Number(e.target.value);
+  $$('.ball').forEach((ball, index) => {
+    inc = (index + 1) * movement / 5000;
+    ball.style.animationDelay = `${inc}s`;
+    ball.style.animationDuration = `${duration}s`;
+  });
+});
+
+$('#duration').addEventListener('input', e => {
+  duration = Number(e.target.value);
+  $$('.ball').forEach((ball, index) => {
+    inc = (index + 1) * movement / 5000;
+    ball.style.animationDelay = `${inc}s`;
+    ball.style.animationDuration = `${duration}s`;
+  });
+});

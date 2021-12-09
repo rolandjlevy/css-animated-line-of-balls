db5635cbc7f243f60e50c8dcad482ceac1357ada
Roland Levy <38256994+rolandjlevy@users.noreply.github.com>
2021-12-09 00:42:52 +0000

    Added fade-in

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const $ = (elem) => document.querySelector(elem);
 const $$ = (elem) => document.querySelectorAll(elem);
 
-const totalBalls = 25;
+const totalBalls = 20;
 const ballSize = 20;
 
 let movement = Number($('#movement').value);
@@ -9,8 +9,9 @@ let duration = Number($('#duration').value);
 let count = 0;
 let inc;
 
+
 while (count++ < totalBalls) {
-  inc = count * movement / 5000;
+  inc = count * movement / 500;
   const ball = document.createElement('div');
   const options = {
     id: count,
@@ -26,20 +27,20 @@ while (count++ < totalBalls) {
   $('.container').appendChild(ball);
 }
 
-$('#movement').addEventListener('input', e => {
-  movement = Number(e.target.value);
+const updateAnimation = () => {
   $$('.ball').forEach((ball, index) => {
-    inc = (index + 1) * movement / 5000;
+    inc = (index + 1) * movement / 500;
     ball.style.animationDelay = `${inc}s`;
     ball.style.animationDuration = `${duration}s`;
   });
+}
+
+$('#movement').addEventListener('input', e => {
+  movement = Number(e.target.value);
+  updateAnimation();
 });
 
 $('#duration').addEventListener('input', e => {
   duration = Number(e.target.value);
-  $$('.ball').forEach((ball, index) => {
-    inc = (index + 1) * movement / 5000;
-    ball.style.animationDelay = `${inc}s`;
-    ball.style.animationDuration = `${duration}s`;
-  });
+  updateAnimation();
 });

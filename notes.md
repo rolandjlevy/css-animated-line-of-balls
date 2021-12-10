ae65d58c7b13e71504b53a14e39c081f8900752e
Roland Levy <38256994+rolandjlevy@users.noreply.github.com>
2021-12-10 23:09:44 +0000

    Adjusted colours

--- notes.md
+++ notes.md
@@ -4,6 +4,12 @@
 
 ```css
   background: radial-gradient(circle at 65% 15%, white 1px, #ebff36 3%, #324700 60%, #ebff36 100%);
+  background: radial-gradient(circle at 25% 70%, 
+    white 1px,
+    hsl(60, 100%, 60%) 5%, 
+    hsl(40, 100%, 20%) 65%, 
+    hsl(60, 100%, 60%) 125%
+  );
 ```
 
 ```js
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const $ = (elem) => document.querySelector(elem);
 const $$ = (elem) => document.querySelectorAll(elem);
 
-const totalBalls = 25;
+const totalBalls = 30;
 const ballSize = 15;
 
 let movement = Number($('#movement').value);
@@ -9,10 +9,18 @@ let duration = Number($('#duration').value);
 let count = 0;
 let inc;
 
+const getBackground = (hue) => {
+  return `radial-gradient(circle at 25% 70%, 
+    hsl(${hue}, 100%, 60%) 5%, 
+    hsl(${hue - 20}, 100%, 20%) 65%, 
+    hsl(${hue}, 100%, 60%) 125%
+  )`;
+}
 
 while (count++ < totalBalls) {
-  inc = count * movement / 500;
+  inc = count * movement / 250;
   const ball = document.createElement('div');
+  const background = getBackground(60 + count * 10);
   const options = {
     id: count,
     className: 'ball',
@@ -21,15 +29,16 @@ while (count++ < totalBalls) {
       height: ${ballSize * 1.25}px;
       animation-delay: ${inc}s;
       animation-duration: ${duration}s;
+      background: ${background};
     `
   }
   Object.assign(ball, options);
   $('.container').appendChild(ball);
 }
-
+ 
 const updateAnimation = () => {
   $$('.ball').forEach((ball, index) => {
-    inc = (index + 1) * movement / 500;
+    inc = (index + 1) * movement / 250;
     ball.style.animationDelay = `${inc}s`;
     ball.style.animationDuration = `${duration}s`;
   });
